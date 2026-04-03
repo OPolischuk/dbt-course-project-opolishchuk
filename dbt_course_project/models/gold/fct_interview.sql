0f978608522eebe1a64f37169dfeacb3c70edcdb
Oleksandr Polishchuk <oleksandr.polishchuk.external@zalando.de>
2026-04-03 11:07:50 +0300

    fct_interview stopped using the Join with dim_candidates

--- dbt_course_project/models/gold/fct_interview.sql
+++ dbt_course_project/models/gold/fct_interview.sql
@@ -7,6 +7,11 @@ WITH pivoted AS (
     SELECT * FROM {{ ref('int_interviews_pivoted') }}
 ),
 
+-- all history for candidate
+candidates_history AS (
+    SELECT * FROM {{ ref('stg_candidates') }}
+),
+
 final AS (
     SELECT
         calc.id,
@@ -20,10 +25,6 @@ final AS (
         stg.run_type,
         stg.interview_type AS type,
         stg.media_status,
-
-
-
-        -- pivot
         stg.created_at AS created_datetime,
         calc.requested_datetime,
         calc.scheduled_datetime,
@@ -32,16 +33,11 @@ final AS (
         calc.finished_datetime AS completed_datetime,
         calc.cancelled_datetime,
         cand.english_level,
-
-        -- 1. interview_duration: IN_PROGRESS -> PENDING_FEEDBACK (online only)
         cand.staffing_status,
-
-        -- 2. feedback_delay: PENDING_FEEDBACK -> COMPLETED
         jobs.base_name,
-
-        -- (Silver)
         jobs.category,
         CAST(stg.created_at AS DATE) AS created_date,
+
         GREATEST(
             CASE
                 WHEN LOWER(stg.run_type) = 'online'
@@ -55,6 +51,7 @@ final AS (
             END,
             0
         ) AS interview_duration,
+
         GREATEST(
             DATEDIFF(
                 'minute',
@@ -68,13 +65,16 @@ final AS (
     LEFT JOIN {{ ref('stg_interviews') }} AS stg
         ON calc.id = stg.interview_id
 
-    LEFT JOIN {{ ref('dim_candidates') }} AS cand
-        ON calc.candidate_id = cand.id
+    -- Join with all candidate's history
+    LEFT JOIN candidates_history AS cand
+        ON
+            calc.candidate_id = cand.candidate_id
+            AND stg.created_at >= cand.valid_from_datetime
+            AND stg.created_at < cand.valid_to_datetime
 
     LEFT JOIN {{ ref('dim_job_functions') }} AS jobs
         ON cand.job_function_id = jobs.id
 
-    -- last record of staging
     QUALIFY
         ROW_NUMBER() OVER (PARTITION BY calc.id ORDER BY stg.updated_at DESC)
         = 1
